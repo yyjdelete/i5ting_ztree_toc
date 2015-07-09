3f18263ef18ba5d176615eb4c24d23f08b6ccdca
SilverFox <yyjdelete@126.com>
2015-07-09 11:24:07 +0800

    Code Format -- js缩进修正

--- src/ztree_toc.js
+++ src/ztree_toc.js
@@ -1,3 +1,4 @@
+
 /**
  * 1.1.1 = 1*100*100 + 1*100 + 1
  * 1.2.2 = 1*100*100 + 2*100 + 3
@@ -6,9 +7,9 @@
  */
 function encode_id_with_array(opts,arr) {
 	var result = 0;
-  for(var z = 0; z < arr.length; z++ ) {
+	for(var z = 0; z < arr.length; z++ ) {
 		result += factor(opts, arr.length - z ,arr[z]);
-  }
+	}
 
 	return result;
 }
@@ -26,16 +27,16 @@ function encode_id_with_array(opts,arr) {
 function get_parent_id_with_array(opts,arr) {
 	var result_arr = [];
 
-  for(var z = 0; z < arr.length; z++ ) {
+	for(var z = 0; z < arr.length; z++ ) {
 		result_arr.push(arr[z]);
-  }
+	}
 
 	result_arr.pop();
 
 	var result = 0;
-  for(var z = 0; z < result_arr.length; z++ ) {
+	for(var z = 0; z < result_arr.length; z++ ) {
 		result += factor(opts,result_arr.length - z,result_arr[z]);
-  }
+	}
 
 	return result;
 }
@@ -74,7 +75,7 @@ function factor(opts ,count,current) {
 		var t = $(opts._zTree);
 		t = $.fn.zTree.init(t,opts.ztreeSetting,opts._header_nodes).expandAll(opts.is_expand_all);
 		// alert(opts._headers * 88);
-		// $(opts._zTree).height(opts._headers  * 33 + 33);
+		// $(opts._zTree).height(opts._headers * 33 + 33);
 
 		if(opts.is_posion_top == true){
 			opts.ztreeStyle.top = '0px';
@@ -102,8 +103,8 @@ function factor(opts ,count,current) {
 			opts._headers[level - 1] ++;
 		} else if(opts._headers.length < level) {
 			for(var i = 0; i < (level - opts._headers.length); i++) {
-			  // console.log('push 1');
-			  opts._headers.push(1);
+				// console.log('push 1');
+				opts._headers.push(1);
 			}
 		}
 
@@ -142,7 +143,7 @@ function factor(opts ,count,current) {
 			anchor = _get_anchor_from_head(header_obj);
 		}
 		
-    // 设置锚点id
+		// 设置锚点id
 		$(header_obj).attr('id',anchor);
 
 		log($(header_obj).text());
@@ -166,7 +167,7 @@ function factor(opts ,count,current) {
 	 */
 	function bind_scroll_event_and_update_postion(opts) {
 		var timeout;
-	    var highlight_on_scroll = function(e) {
+		var highlight_on_scroll = function(e) {
 			if (timeout) {
 				clearTimeout(timeout);
 			}
@@ -183,17 +184,17 @@ function factor(opts ,count,current) {
 						$('a').removeClass('curSelectedNode');
 
 						// 由于有root节点，所以i应该从1开始
-				  		var obj = $('#tree_' + (i+1) + '_a').addClass('curSelectedNode');
+						var obj = $('#tree_' + (i+1) + '_a').addClass('curSelectedNode');
 						break;
 					}
 				}
 			}, opts.refresh_scroll_time);
 		};
 
-	  if (opts.highlight_on_scroll) {
-	    $(opts.scroll_selector).bind('scroll', highlight_on_scroll);
-	    highlight_on_scroll();
-	  }
+		if (opts.highlight_on_scroll) {
+			$(opts.scroll_selector).bind('scroll', highlight_on_scroll);
+			highlight_on_scroll();
+		}
 	}
 
 	/*
@@ -230,7 +231,7 @@ function factor(opts ,count,current) {
 			render_with_ztree(opts);
 
 			// 根据滚动确定当前位置，并更新ztree
-		    bind_scroll_event_and_update_postion(opts);
+			bind_scroll_event_and_update_postion(opts);
 		});
 		// each end
 	}
@@ -249,7 +250,7 @@ function factor(opts ,count,current) {
 		 * 此选项默认是false，不开启
 		 */
 		use_head_anchor: false,
-    scroll_selector: 'window',
+		scroll_selector: 'window',
 		highlight_offset: 0,
 		highlight_on_scroll: true,
 		/*
@@ -308,7 +309,7 @@ function factor(opts ,count,current) {
 					}
 					if($.fn.ztree_toc.defaults.is_highlight_selected_line == true) {
 						$('#' + treeNode.id).css('color' ,'red').fadeOut("slow" ,function() {
-						    // Animation complete.
+							// Animation complete.
 							$(this).show().css('color','black');
 						});
 					}
